5dc0571596102fd1b0c8d12d7405a9f387bfb431
Moomooo95 <mohamed.ali.benzina.pro@gmail.com>
2023-01-29 02:49:43 +0100

    debug build workflow

---
+++ src/rust/mmrcms/sources/frscan/src/lib.rs
@@ -0,0 +1,50 @@
+#![no_std]
+use mmrcms_template::{mmrcms, template::MMRCMSSource};
+
+mmrcms! {
+	MMRCMSSource {
+		base_url: "https://frscan.ws",
+		lang: "fr",
+		category_mapper: |idx| {
+			String::from(match idx {
+				1 => "1", // Action
+				2 => "2", // Adventure
+				3 => "3", // Comedy
+				4 => "4", // Doujinshi
+				5 => "5", // Drama
+				6 => "6", // Ecchi
+				7 => "7", // Fantasy
+				8 => "8", // Gender Bender
+				9 => "9", // Harem
+				10 => "10", // Historical
+				11 => "11", // Horror
+				12 => "12", // Josei
+				13 => "13", // Martial Arts
+				14 => "14", // Mature
+				15 => "15", // Mecha
+				16 => "16", // Mystery
+				17 => "17", // One Shot
+				18 => "18", // Psychological
+				19 => "19", // Romance
+				20 => "20", // Vie Scolaire
+				21 => "21", // Sci-fi
+				22 => "22", // Seinen
+				23 => "23", // Shoujo
+				24 => "24", // Shoujo Ai
+				25 => "25", // Shounen
+				26 => "26", // Shounen Ai
+				27 => "27", // Tranche de vie
+				28 => "28", // Sports
+				29 => "29", // Supernatural
+				30 => "30", // Tragedie
+				31 => "31", // Yaoi
+				32 => "32", // Yuri
+				33 => "33", // Autre
+				34 => "34", // BD Occidentale
+				35 => "35", // Webtoon
+				_ => "",
+			})
+		},
+		..Default::default()
+	}
+}
