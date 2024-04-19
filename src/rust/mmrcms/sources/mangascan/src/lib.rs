8ab2a86d1eed014eb8d1650d1b5bd6972956ccc9
Moomooo95 <mohamed.ali.benzina.pro@gmail.com>
2024-04-20 01:43:36 +0200

    - remove lelscanvf and frscan sources + add mangascan source

--- src/rust/mmrcms/sources/mangascan/src/lib.rs
+++ src/rust/mmrcms/sources/mangascan/src/lib.rs
@@ -3,42 +3,44 @@ use mmrcms_template::{mmrcms, template::MMRCMSSource};
 
 mmrcms! {
 	MMRCMSSource {
-		base_url: "https://lelscanvf.cc",
+		base_url: "http://mangascan-fr.com",
 		lang: "fr",
 		category_mapper: |idx| {
 			String::from(match idx {
 				1 => "1", // Action
 				2 => "2", // Adventure
-				3 => "3", // Comedy
+				3 => "3", // Comédie
 				4 => "4", // Doujinshi
-				5 => "5", // Drama
+				5 => "5", // Drame
 				6 => "6", // Ecchi
 				7 => "7", // Fantasy
-				8 => "8", // Gender Bender
+				8 => "8", // Webtoon
 				9 => "9", // Harem
-				10 => "10", // Historical
-				11 => "11", // Horror
-				12 => "12", // Josei
-				13 => "13", // Martial Arts
+				10 => "10", // Historique
+				11 => "11", // Horreur
+				12 => "12", // Thriller
+				13 => "13", // Arts Martiaux
 				14 => "14", // Mature
-				15 => "15", // Mecha
-				16 => "16", // Mystery
+				15 => "15", // Tragique
+				16 => "16", // Mystère
 				17 => "17", // One Shot
-				18 => "18", // Psychological
+				18 => "18", // Psychologique
 				19 => "19", // Romance
 				20 => "20", // School Life
-				21 => "21", // Sci-fi
+				21 => "21", // Science-fiction
 				22 => "22", // Seinen
-				23 => "23", // Shoujo
+				23 => "23", // Erotique
 				24 => "24", // Shoujo Ai
 				25 => "25", // Shounen
 				26 => "26", // Shounen Ai
 				27 => "27", // Slice of Life
 				28 => "28", // Sports
-				29 => "29", // Supernatural
+				29 => "29", // Surnaturel
 				30 => "30", // Tragedy
-				31 => "31", // Yaoi
-				32 => "32", // Yuri
+				31 => "31", // Gangster
+				32 => "32", // Crime
+				33 => "33", // Biographique
+				34 => "34", // Fantastique
 				_ => "",
 			})
 		},
