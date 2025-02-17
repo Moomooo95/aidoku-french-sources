543e3965e601fe8d417e3de61c0e0c5417a1d6e9
Moomooo95 <mohamed.ali.benzina.pro@gmail.com>
2025-02-17 23:05:25 +0100

    update user-agent, filters, selector for mangascantrad source

--- src/rust/madara/sources/mangascantrad/src/lib.rs
+++ src/rust/madara/sources/mangascantrad/src/lib.rs
@@ -9,6 +9,7 @@ fn get_data() -> template::MadaraSiteData {
 	let data: template::MadaraSiteData = template::MadaraSiteData {
 		base_url: String::from("https://manga-scantrad.io"),
 		lang: String::from("fr"),
+		description_selector: String::from("div.description-summary .summary__content"),
 		date_format: String::from("d MMMM yyyy"),
 		status_filter_ongoing: String::from("En cours"),
 		status_filter_completed: String::from("Terminé"),
@@ -18,7 +19,7 @@ fn get_data() -> template::MadaraSiteData {
 		trending: String::from("Tendance"),
 		status: |html| {
 			let status_str = html
-				.select("div.post-content_item:contains(État) div.summary-content")
+				.select("div.post-content_item:contains(Status) div.summary-content")
 				.text()
 				.read()
 				.trim()
@@ -32,7 +33,7 @@ fn get_data() -> template::MadaraSiteData {
 			}
 		},
 		alt_ajax: true,
-		user_agent: Some(String::from("Mozilla/5.0 (iPhone; CPU iPhone OS 17_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3.1 Mobile/15E148 Safari/604.1")),
+		user_agent: Some(String::from("Mozilla/5.0 (iPhone; CPU iPhone OS 17_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) GSA/300.0.598994205 Mobile/15E148 Safari/604")),
 		..Default::default()
 	};
 	data
